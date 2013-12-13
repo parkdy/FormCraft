337b3dbb6d3dc99e21e3d0eae53858392f045e72
David Park <parkdy6@gmail.com>
2013-12-12 23:31:38 -0500

    Add form editor buttons and tabs

--- app/controllers/form_editors_controller.rb
+++ app/controllers/form_editors_controller.rb
@@ -1,6 +1,6 @@
 class FormEditorsController < ApplicationController
   def index
     @mode = params[:mode] || "new"
-    @form = (params[:form_id] ? Form.find(params[:form_id]) : Form.new)
+    @form = (params[:form_id] ? Form.find(params[:form_id]) : Form.new(title: "Untitled Form"))
   end
 end
