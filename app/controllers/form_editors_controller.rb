63aa577df9e28ae5330c794f1dc6acaf2ef756ac
David Park <parkdy6@gmail.com>
2013-12-13 13:11:02 -0500

    Refactor form editor view

--- app/controllers/form_editors_controller.rb
+++ app/controllers/form_editors_controller.rb
@@ -1,7 +1,8 @@
 class FormEditorsController < ApplicationController
   def index
+    @params = params
     @mode = params[:mode] || "new"
     @form = (params[:form_id] ? Form.find(params[:form_id]) : Form.new(title: "Untitled Form"))
-    @editor_tab = "add_field"
+    @editor_tab = params[:editor_tab] || "add_field"
   end
 end
