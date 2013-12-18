3e5d60c0385f3a57f3eb9d769e63a4f707fb1f05
David Park <parkdy6@gmail.com>
2013-12-18 15:23:33 -0500

    Add field options in editor

--- app/assets/javascripts/views/form_editors/field_settings.js
+++ app/assets/javascripts/views/form_editors/field_settings.js
@@ -7,7 +7,8 @@ FormBuilder.Views.FormEditorsFieldSettings = Backbone.View.extend({
 
   events: {
     "blur .settings_field": "updateField",
-    "click #update_settings_btn": "dummyButton"
+    "click #update_settings_btn": "dummyButton",
+    "click #add_option_btn": "addFieldOption"
   },
 
   render: function() {
@@ -20,7 +21,7 @@ FormBuilder.Views.FormEditorsFieldSettings = Backbone.View.extend({
     // Settings form fields
     var settingsFields = new FormBuilder.Collections.Fields([
       { field_type: "text", name: "label", label: "Label:", default: this.field.get('label') },
-      { field_type: "text", name: "name", label: "Name (must be unique):", default: this.field.get('name') }
+      { field_type: "text", name: "name", label: "Name (unique):", default: this.field.get('name') }
     ]);
 
     // Add default value settings
@@ -41,25 +42,10 @@ FormBuilder.Views.FormEditorsFieldSettings = Backbone.View.extend({
     var fieldAttr = $(event.target).closest('.settings_field').attr('data-name');
     var value = $(event.target).val();
 
-    // Default value for checkbox fields
-    var options = this.field.get('field_options');
-
     if (fieldAttr == 'default' && this.field.get('field_type') == 'checkbox') {
-
-      // Wrap default value settings field in form
-      var $form = $('<form></form>');
-      $form.html($(event.target).closest('.settings_field').clone());
-
-      // Grab default values from form
-      var defaultValues = $form.serializeJSON().default;
-      defaultValues.shift();
-
-      // Set field options' default properties
-      options.each(function(option) {
-        var isDefault = (defaultValues.indexOf(option.get('value')) > -1);
-        option.set('default', isDefault);
-      });
-
+      this.updateCheckboxDefault(event);
+    } else if (fieldAttr == 'field_options_settings') {
+      this.updateFieldOptionsSettings(event);
     } else {
       // No field options
       this.field.set(fieldAttr, value);
@@ -70,6 +56,54 @@ FormBuilder.Views.FormEditorsFieldSettings = Backbone.View.extend({
 
   dummyButton: function(event) {
     event.preventDefault();
+  },
+
+  updateCheckboxDefault: function(event) {
+    var options = this.field.get('field_options');
+
+    // Wrap default value settings field in form
+    var $form = $('<form></form>');
+    $form.html($(event.target).closest('.settings_field').clone());
+
+    // Grab default values from form
+    var defaultValues = $form.serializeJSON().default;
+    defaultValues.shift();
+
+    // Set field options' default properties
+    options.each(function(option) {
+      var isDefault = (defaultValues.indexOf(option.get('value')) > -1);
+      option.set('default', isDefault);
+    });
+  },
+
+  updateFieldOptionsSettings: function(event) {
+    var options = this.field.get('field_options');
+
+    // Wrap options settings in form
+    var $form = $('<form></form>');
+    $form.html($(event.target).closest('.settings_field').clone());
+
+    // Grab options settings from form
+    var optionsSettings = $form.serializeJSON();
+
+    // Find option by cid and set its properties
+    for (var cid in optionsSettings) {
+      var option = options.get({cid: cid});
+      option.set(optionsSettings[cid])
+    };
+
+    FormBuilder.formEditorsIndex.renderEditorView({field: this.field});
+  },
+
+  addFieldOption: function(event) {
+    event.preventDefault();
+
+    var options = this.field.get('field_options');
+    var optionValue = (options.length+1).toString();
+    var newOption = new FormBuilder.Models.FieldOption({label: "Option"+optionValue, value: optionValue});
+    options.add(newOption);
+
+    FormBuilder.formEditorsIndex.renderEditorView({field: this.field});
   }
 
 });
